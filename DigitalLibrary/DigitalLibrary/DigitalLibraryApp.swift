16b01b1e3c39925c17c15f76c64b25c874c000fb
Desislava Dimitrova <desislava.d.dimitrova@paysafe.com>
2024-01-23 23:09:22 +0200

    Project created

---
+++ DigitalLibrary/DigitalLibrary/DigitalLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalLibraryApp.swift
+//  DigitalLibrary
+//
+//  Created by Desislava D. Dimitrova on 23.01.24.
+//
+
+import SwiftUI
+
+@main
+struct DigitalLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
