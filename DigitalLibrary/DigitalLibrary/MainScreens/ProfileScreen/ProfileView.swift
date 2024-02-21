0622fb889aa6ccc85de7c09bf1d70f7bd2357461
Ognyan Yonchev <ognyan.yonchev@paysafe.com>
2024-02-21 20:00:15 +0200

    Add scroll and fix the fields in profile screen

--- DigitalLibrary/DigitalLibrary/MainScreens/ProfileScreen/ProfileView.swift
+++ DigitalLibrary/DigitalLibrary/MainScreens/ProfileScreen/ProfileView.swift
@@ -11,56 +11,70 @@ struct ProfileView: View {
     @ObservedObject var viewModel: ProfileViewModel
 
     var body: some View {
-        VStack {
+        ScrollView {
             VStack {
-                Image(systemName: "person.crop.circle.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 100, height: 100)
-                    .clipped()
-                    .cornerRadius(50)
-                    .foregroundColor(.purple.opacity(0.6))
+                VStack {
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                        .cornerRadius(50)
+                        .foregroundColor(.purple.opacity(0.6))
 
-                Text("Profile")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.purple.opacity(0.6))
-            }
-            .frame(maxWidth: .infinity)
+                    Text("Profile")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.purple.opacity(0.6))
+                }
+                .frame(maxWidth: .infinity)
 
-            ProfileInfoField(viewModel: .init(label: "First name",
-                                              text: $viewModel.firstName,
-                                              fieldType: .firstName,
-                                              editingField: $viewModel.editingField,
-                                              onCommit: { viewModel.updateUserData(field: .firstName) }))
-            ProfileInfoField(viewModel: .init(label: "Last name",
-                                              text: $viewModel.lastName,
-                                              fieldType: .lastName,
-                                              editingField: $viewModel.editingField,
-                                              onCommit: { viewModel.updateUserData(field: .lastName) }))
-            ProfileInfoField(viewModel: .init(label: "Phone",
-                                              text: $viewModel.phone,
-                                              fieldType: .phone,
-                                              editingField: $viewModel.editingField,
-                                              onCommit: { viewModel.updateUserData(field: .phone) }))
-            Button("Sign Out") {
-                viewModel.signOut()
-            }
-            .alert(viewModel.alertMessage, isPresented: $viewModel.showingAlert) {
-                Button("OK", role: .cancel) { }
+                ProfileInfoField(viewModel: .init(label: "First name",
+                                                  text: $viewModel.firstName,
+                                                  fieldType: .firstName,
+                                                  editingField: $viewModel.editingField,
+                                                  onCommit: { viewModel.updateUserData(field: .firstName) }))
+                .submitLabel(.done)
+                .keyboardType(.namePhonePad)
+                .autocapitalization(.none)
+
+                ProfileInfoField(viewModel: .init(label: "Last name",
+                                                  text: $viewModel.lastName,
+                                                  fieldType: .lastName,
+                                                  editingField: $viewModel.editingField,
+                                                  onCommit: { viewModel.updateUserData(field: .lastName) }))
+                .submitLabel(.done)
+                .keyboardType(.namePhonePad)
+                .autocapitalization(.none)
+
+                ProfileInfoField(viewModel: .init(label: "Phone",
+                                                  text: $viewModel.phone,
+                                                  fieldType: .phone,
+                                                  editingField: $viewModel.editingField,
+                                                  onCommit: { viewModel.updateUserData(field: .phone) }))
+                .submitLabel(.done)
+                .keyboardType(.numberPad)
+                .autocapitalization(.none)
+
+                Button("Sign Out") {
+                    viewModel.signOut()
+                }
+                .alert(viewModel.alertMessage, isPresented: $viewModel.showingAlert) {
+                    Button("OK", role: .cancel) { }
+                }
+                .padding()
+                .frame(width: 310, height: 50)
+                .background(.purple.opacity(0.9))
+                .foregroundColor(.white)
+                .cornerRadius(30)
+                .padding()
+
+                Spacer()
             }
             .padding()
-            .frame(width: 310, height: 50)
-            .background(.purple.opacity(0.9))
-            .foregroundColor(.white)
-            .cornerRadius(30)
-            .padding()
-
-            Spacer()
-        }
-        .padding()
-        .onAppear {
-            viewModel.fetchCurrentUser()
+            .onAppear {
+                viewModel.fetchCurrentUser()
+            }
         }
     }
 }
@@ -68,7 +82,7 @@ struct ProfileView: View {
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView(viewModel: ProfileViewModel(appRootManager: AppRootManager(),
-            authenticationProvider: AuthenticationRepositoryMock(),
-            usersProvider: UserRepositoryMock()))
+                                                authenticationProvider: AuthenticationRepositoryMock(),
+                                                usersProvider: UserRepositoryMock()))
     }
 }
